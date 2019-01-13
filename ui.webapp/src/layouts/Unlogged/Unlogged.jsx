faeb501a20af598e173e4dd59ce8df5f727a16c9
haroldjose <haroldbarros@gmail.com>
2019-01-13 06:30:45 -0200

    webapp

---
+++ ui.webapp/src/layouts/Unlogged/Unlogged.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+
+import Footer from "../../components/Footer/Footer";
+import loginRoutes from "../../routes/login";
+
+var ps;
+
+class Unlogged extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      backgroundColor: "black",
+      activeColor: "info",
+    }
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.refs.mainPanel);
+      document.body.classList.toggle("perfect-scrollbar-on");
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
+      document.body.classList.toggle("perfect-scrollbar-on");
+    }
+  }
+  componentDidUpdate(e) {
+    if (e.history.action === "PUSH") {
+      this.refs.mainPanel.scrollTop = 0;
+      document.scrollingElement.scrollTop = 0;
+    }
+  }
+  handleActiveClick = (color) => {
+    this.setState({ activeColor: color });
+  }
+  handleBgClick = (color) => {
+    this.setState({ backgroundColor: color });
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="main-panel">
+          <Switch>
+            {loginRoutes.map((prop, key) => {
+              if (prop.pro) {
+                return null;
+              }
+              if (prop.redirect) {
+                return <Redirect from={prop.path} to={prop.pathTo} key={key} />;
+              }
+              return (
+                <Route path={prop.path} component={prop.component} key={key} />
+              );
+            })}
+          </Switch>
+          <Footer fluid />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Unlogged;
